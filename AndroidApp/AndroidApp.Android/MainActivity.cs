5d58c04c4415ec62ce7df750086e50adb41d2b7b
selfcac <selfcac@5gbinbox.com>
2019-10-31 22:19:12 +0200

    package name + app name

--- AndroidApp/AndroidApp.Android/MainActivity.cs
+++ AndroidApp/AndroidApp.Android/MainActivity.cs
@@ -9,7 +9,7 @@ using Android.OS;
 
 namespace AndroidApp.Droid
 {
-    [Activity(Label = "AndroidApp", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
+    [Activity(Label = "selfcac", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
     public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
     {
         protected override void OnCreate(Bundle savedInstanceState)
