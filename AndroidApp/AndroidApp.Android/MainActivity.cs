72749d4f3db8030b3accfa84f287dd1651401731
selfcac <selfcac@5gbinbox.com>
2019-11-05 21:34:19 +0200

    run with permission (Must be in activity!)

--- AndroidApp/AndroidApp.Android/MainActivity.cs
+++ AndroidApp/AndroidApp.Android/MainActivity.cs
@@ -6,11 +6,12 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using Android.Support.V4.App;
 
 namespace AndroidApp.Droid
 {
     [Activity(Label = "selfcac", Icon = "@mipmap/icon", Theme = "@style/MainTheme", MainLauncher = true, ConfigurationChanges = ConfigChanges.ScreenSize | ConfigChanges.Orientation)]
-    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity
+    public class MainActivity : global::Xamarin.Forms.Platform.Android.FormsAppCompatActivity, ActivityCompat.IOnRequestPermissionsResultCallback
     {
         protected override void OnCreate(Bundle savedInstanceState)
         {
@@ -21,5 +22,56 @@ namespace AndroidApp.Droid
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
             LoadApplication(new App());
         }
-    }
+
+        JavaDictionary<int, Action> _requestedActionUnderPermissions = new JavaDictionary<int, Action>();
+        public void RunUnderPermission(Action callback, string[] permissions)
+        {
+            bool allAllowed = true;
+            foreach (string p in permissions)
+            {
+                if (CheckSelfPermission(p) == Permission.Denied)
+                {
+                    allAllowed = false;
+                    break;
+                }
+            }
+
+            if (allAllowed)
+            {
+                callback?.Invoke();
+            }
+            else
+            {
+                int reqCode = callback.GetHashCode();
+                _requestedActionUnderPermissions.Add(reqCode, callback);
+                ActivityCompat.RequestPermissions(this, permissions , reqCode);
+            }
+        }
+
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
+        {
+            if (_requestedActionUnderPermissions.ContainsKey(requestCode))
+            {
+                Action callback = _requestedActionUnderPermissions[requestCode];
+                _requestedActionUnderPermissions.Remove(requestCode);
+
+                bool allAllowed = true;
+                for (Permission p in grantResults)
+                {
+                    if (p == Permission.Denied)
+                    {
+                        allAllowed = false;
+                        break;
+                    }
+                }
+
+                if (allAllowed)
+                {
+                    callback?.Invoke();
+                }
+            }
+        }
+
+
+    } 
 }
