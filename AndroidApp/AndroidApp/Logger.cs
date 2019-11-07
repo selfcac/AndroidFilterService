3f536234badeaedbaa22d492a218ff612656fd47
selfcac <selfcac@5gbinbox.com>
2019-11-07 23:33:19 +0200

    logger for app level

--- AndroidApp/AndroidApp/Logger.cs
+++ AndroidApp/AndroidApp/Logger.cs
@@ -1,14 +1,66 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Text;
 
 namespace AndroidApp
 {
     class Logger
     {
+        const char debug_char = 'D';
+        const char error_char = 'E';
+        const string logFormat = "log_{0}.txt";
+
+        public const string TAG = "logger_app";
+
         private static string lineFormat(string myTag, char Level, string message)
         {
             return string.Format("[{0}] {1}/{2} {3}", DateTime.Now, Level, myTag, message);
         }
+
+        private static void logToPublicFile(string filename, string TAG, char level, string message)
+        {
+            try
+            {
+                File.WriteAllLines(
+                        AndroidBridge.GetAbsulotePath(filename, true),
+                        new[] { lineFormat(TAG, level, message) }
+                        );
+
+                switch (level)
+                {
+                    case debug_char:
+                        AndroidBridge.d(TAG, message);
+                        break;
+
+                    case error_char:
+                        AndroidBridge.e(TAG, message);
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                AndroidBridge.e(Logger.TAG, ex);
+            }
+        }
+
+        public static void d(string TAG, string message)
+        {
+            char myChar = debug_char;
+            logToPublicFile(string.Format(logFormat, myChar),TAG, myChar, message);
+        }
+
+        public static void e(string TAG, string message)
+        {
+            char myChar = error_char;
+            logToPublicFile(string.Format(logFormat, myChar), TAG, myChar, message);
+        }
+
+        public static void e(string TAG, Exception ex)
+        {
+            e(TAG, ex.ToString());
+        }
+
+
     }
 }
