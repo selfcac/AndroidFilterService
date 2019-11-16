06d150d1eab30fa1d450970e905d3abe5badcf4c
selfcac <selfcac@5gbinbox.com>
2019-11-17 00:40:51 +0200

    starting TimeLock

---
+++ AndroidApp/AndroidApp/FilterUtils/TaskResult.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AndroidApp.FilterUtils
+{
+    class TaskResult
+    {
+        public bool success;
+        public string eventReason;
+
+        public static TaskResult Fail(string reason)
+        {
+            return new TaskResult()
+            {
+                success = false,
+                eventReason = reason
+            };
+        }
+
+        public static TaskResult Success(string reason)
+        {
+            return new TaskResult()
+            {
+                success = true,
+                eventReason = reason
+            };
+        }
+
+        public static implicit operator bool(TaskResult e)
+        {
+            return e.success;
+        }
+    }
+
+    class TaskResultExtra<T> : TaskResult
+    {
+        public T result;
+
+        public static TaskResultExtra<T> setResult(T resultData)
+        {
+            return new TaskResultExtra<T>()
+            {
+                success = true,
+                result = resultData
+            };
+        }
+
+        public static implicit operator T(TaskResultExtra<T> o)
+        {
+            return o.result;
+        }
+    }
+
+
+
+
+}
---
+++ AndroidApp/AndroidApp/FilterUtils/TimeLock.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Text;
+
+namespace AndroidApp.FilterUtils
+{
+    class TimeLock
+    {
+        static readonly string TAG = typeof(TimeLock).Name.ToString();
+
+        bool isLocked()
+        {
+            string filename = "";
+            string result ="No issue (init)"; // unlocked on error by default
+            bool isLocked = true;
+
+            try
+            {
+                if (File.Exists(filename))
+                {
+                    DateTime unlock = DateTime.Now.Subtract(TimeSpan.FromMinutes(1));
+                    if (DateTime.TryParse(File.ReadAllText(filename), out unlock))
+                    {
+                        if (unlock > DateTime.Now)
+                        {
+                            isLocked = true;
+                        }
+                        else
+                        {
+                            isLocked = false;
+                            result = "Lock expired";
+                        }
+                    }
+                }
+                else
+                {
+                    isLocked = false;
+                    result = "File doesn't exits";
+                }
+            }
+            catch (Exception ex)
+            {
+                isLocked = false;
+                result = ex.ToString();
+            }
+
+            return isLocked;
+        }
+    }
+}
