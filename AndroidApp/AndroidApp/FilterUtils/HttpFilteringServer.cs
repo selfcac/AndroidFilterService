1b6e15208b64b85159e55d73cdd3e19f62564e7d
selfcac <selfcac@5gbinbox.com>
2019-11-24 02:36:49 +0200

    Resources + HttpFilteringServer old code for spinner

--- AndroidApp/AndroidApp/FilterUtils/HttpFilteringServer.cs
+++ AndroidApp/AndroidApp/FilterUtils/HttpFilteringServer.cs
@@ -1,24 +1,260 @@
-﻿using System;
+﻿using ISimpleHttpListener.Rx.Enum;
+using ISimpleHttpListener.Rx.Model;
+using SimpleHttpListener.Rx.Extension;
+using SimpleHttpListener.Rx.Model;
+using SimpleHttpListener.Rx.Service;
+using System;
 using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Net.Sockets;
+using System.Reactive.Linq;
 using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
 
 namespace AndroidApp.FilterUtils
 {
     class HttpFilteringServer
     {
+        const int port = 8000;
+        static readonly string TAG = typeof(HttpFilteringServer).Name.ToString();
+
+        //https://github.com/1iveowl/SimpleHttpListener.Rx
+        CancellationTokenSource cancellationTokenSource;
+        TcpListener myListener;
+        IDisposable myServerInstance;
+
+        string ipString(IPEndPoint ip)
+        {
+            return ip.Address + "^" + ip.Port;
+        }
+
+        public void Start(int port)
+        {
+            try
+            {
+                myListener = new TcpListener(IPAddress.Loopback, port)
+                {
+                    ExclusiveAddressUse = false
+                };
+
+                var httpSender = new HttpSender();
+
+                cancellationTokenSource = new CancellationTokenSource();
+
+                myServerInstance = myListener.ToHttpListenerObservable(cancellationTokenSource.Token)
+                    .Do(r =>
+                    {
+                        AndroidBridge.d(TAG, "[START] \"" + r.Path + "\" IP: " + ipString(r.RemoteIpEndPoint));
+                    })
+                    // Send reply to browser
+                    .Select(r => Observable.FromAsync(() => SendResponseAsync(r, httpSender)))
+                    .Concat()
+                    .Subscribe(r =>
+                    {
+                        //Console.WriteLine("Reply sent.");                     
+                    },
+                    ex =>
+                    {
+                        AndroidBridge.e(TAG, ex.ToString());
+                    },
+                    () =>
+                    {
+                        AndroidBridge.d(TAG,"Server completed");
+                    });
+            }
+            catch (Exception ex)
+            {
+                AndroidBridge.e(TAG, ex.ToString());
+            }
+        }
+
+        string readReqBodyString(IHttpRequestResponse req)
+        {
+            string result = "";
+            try
+            {
+                if (req.Body != null && req.Body.Length > 0)
+                {
+                    StreamReader reader = new StreamReader(req.Body);
+                    req.Body.Position = 0;
+                    result = reader.ReadToEnd();
+                }
+            }
+            catch (Exception ex)
+            {
+                AndroidBridge.e(TAG, ex.ToString());
+            }
+            return result;
+        }
+
+        string echoBody(IHttpRequestResponse req)
+        {
+            string result = "";
+            try
+            {
+
+                string headers = "";
+                foreach (var key in req.Headers.Keys)
+                {
+                    headers += $"{key}: {req.Headers[key]}\r\n";
+                }
+
+                string sentBody = readReqBodyString(req);
+
+                result =
+                    AndroidApp.Properties.Resources.Echo
+                    .Replace("{0}", headers)
+                    .Replace("{1}", sentBody)
+                    .Replace("{2}", req.Method)
+                    .Replace("{3}", req.Path)
+                    ;
+            }
+            catch (Exception ex)
+            {
+                AndroidBridge.e(TAG, ex.ToString());
+            }
+            return result;
+        }
+
+        string checkDomain(IHttpRequestResponse req)
+        {
+            string result = "";
+            try
+            {
+                var reqJSON = readReqBodyString(req);
+                string domain = SpinResultHelper.getDomainName(reqJSON).Trim(new char[] { ' ', '"' });
+                domainHistory.Add(domain);
+
+                bool block = domain.Contains("yoni");
+                result = SpinResultHelper.domainCategoryResult(block);
+            }
+            catch (Exception ex)
+            {
+                AndroidBridge.e(TAG, ex.ToString());
+            }
+            return result;
+        }
+
+        string stringHTML(string body = "", string title = "")
+        {
+            string result = "";
+            try
+            {
+                result =
+                    AndroidApp.Properties.Resources.EmptyHTML
+                    .Replace("{title}", title)
+                    .Replace("{body}", body)
+                    ;
+            }
+            catch (Exception ex)
+            {
+                AndroidBridge.e(TAG, ex.ToString());
+            }
+            return result;
+        }
+
+        List<string> domainHistory = new List<string>();
+        string domainHistoryList(IHttpRequestResponse req)
+        {
+            string result = "";
+            try
+            {
+                string body = String.Join("<br />", domainHistory);
+                result = stringHTML(body: body);
+            }
+            catch (Exception ex)
+            {
+                AndroidBridge.e(TAG, ex.ToString());
+            }
+            return result;
+        }
+
+        const string ContentHTML = "text/html; charset=UTF-8";
+        const string ContentJSON = "application/json";
+
+        async Task SendResponseAsync(IHttpRequestResponse request, HttpSender httpSender)
+        {
+            try
+            {
+                if (request.RequestType == RequestType.TCP)
+                {
+                    string path = request.Path;
+                    string responseContentType = ContentHTML;
+                    string responseBody = $"<html>\r\n<body>Unknown Path:\r\n{path}</body>\r\n</html>";
+
+
+                    if (path.StartsWith("/echo"))
+                    {
+                        responseContentType = ContentHTML;
+                        responseBody = echoBody(request);
+                    }
+                    else if (path.StartsWith("/history"))
+                    {
+                        responseContentType = ContentHTML;
+                        responseBody = domainHistoryList(request);
+                    }
+                    else if (path.StartsWith("/check"))
+                    {
+                        responseContentType = ContentJSON;
+                        responseBody = checkDomain(request);
+                    }
+
+                    var response = new HttpResponse
+                    {
+                        StatusCode = (int)HttpStatusCode.OK,
+                        ResponseReason = HttpStatusCode.OK.ToString(),
+                        Headers = new Dictionary<string, string>
+                    {
+                        {"Date", DateTime.UtcNow.ToString("r")},
+                        {"Content-Type",  responseContentType},
+                        {"Connection","close" }
+                    },
+                        Body = new MemoryStream(Encoding.UTF8.GetBytes(responseBody))
+                    };
+
+                    AndroidBridge.d(TAG, "[RESP-SEND] \"" + request.Path + "\" IP: " + ipString(request.RemoteIpEndPoint));
+
+                    await httpSender.SendTcpResponseAsync(request, response).ConfigureAwait(false);
+                    request.TcpClient.Close(); // Force close!
+
+                    AndroidBridge.d(TAG, "[END] \"" + request.Path + "\" IP: " + ipString(request.RemoteIpEndPoint));
+                }
+            }
+            catch (Exception ex)
+            {
+                AndroidBridge.e(TAG, ex.ToString());
+            }
+        }
+
+        public void Stop()
+        {
+            try
+            {
+                AndroidBridge.d(TAG, "Closing server");
+                cancellationTokenSource?.Cancel();
+                myServerInstance?.Dispose();
+                myListener?.Stop();
+            }
+            catch (Exception ex)
+            {
+                AndroidBridge.e(TAG, ex.ToString());
+            }
+        }
+
+
+
+
         public void StartHttpServer()
         {
-            HttpFilteringServer.HttpCallbackRouter();
+            Start(port);
         }
 
-        public static void HttpCallbackRouter()
-        {
-
-        }
-
+      
         public void StopHTTPServer()
         {
-
+            Stop();
         }
     }
 }
---
+++ AndroidApp/AndroidApp/FilterUtils/SpinResultHelper.cs
@@ -0,0 +1,61 @@
+﻿using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace AndroidApp.FilterUtils
+{
+    class SpinResultHelper
+    {
+
+        static readonly string TAG = typeof(SpinResultHelper).Name.ToString();
+
+        public static string getDomainName(string jsonBody)
+        {
+            string result = "";
+            try
+            {
+                var jsonObject = JObject.Parse(jsonBody);
+                var version = jsonObject["v"].ToString();
+                if (version == "\"1\"")
+                {
+                    var domainName = jsonObject["domainName"];
+                    result = domainName.ToString();
+                }
+                else
+                {
+                    throw new Exception("Unknown Version of domain request");
+                }
+            }
+            catch (Exception ex)
+            {
+                AndroidBridge.e(TAG, ex.ToString());
+            }
+            return result;
+        }
+
+
+        const int Category_Blocked = 1011;
+        const int Category_Allowed = 1;
+
+        public static string domainCategoryResult(bool block)
+        {
+            string result = "";
+            try
+            {
+                int myCategory = block ? Category_Blocked : Category_Allowed;
+
+                var jsonObject = new JObject();
+                jsonObject["categories"] = new JArray(new[] { new JValue(myCategory) });
+                jsonObject["resultCode"] = new JValue(0);
+
+                result = jsonObject.ToString();
+            }
+            catch (Exception ex)
+            {
+                AndroidBridge.e(TAG, ex.ToString());
+            }
+            return result;
+        }
+    }
+}
